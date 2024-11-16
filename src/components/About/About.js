a74f1c15db6337fa03f42e57bf13bd952b752462
Gauravmane16 <gauravmane2017@gmail.com>
2024-11-16 19:54:41 +0530

    code push

---
+++ src/components/About/About.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+        <ScrollAnimation animateIn="fadeInLeft">
+          <Image
+            src="/profile.jpeg"
+            alt="man-svgrepo"
+          />
+        </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+            Hello! My name is <strong>Gaurav Mane</strong>. I'm originally from Pulgaon (Wardha) and have been doing Full time Job and living in Mumbai for the past Two years. During my free time here, I gained valuable experience through various personal projects, which greatly enhanced my Technical skills , Problem solving skills, and adaptability.
+            </ScrollAnimation>
+
+            <br /><br />
+            
+            <ScrollAnimation animateIn="fadeInLeft">
+            My journey into the world of technology began with a strong passion for problem-solving, which led me to pursue the Electronics and Telecommunication Engineering. Despite as an Electronics Engineer, I remained determined and forged lasting friendships with Coding and IT technologies that supported me throughout the process of becoming a better Developer.
+            Throughout my studies at GCOEJ and my personal learnings, I had the opportunity to engage in fascinating projects.
+            </ScrollAnimation>
+
+            <br /><br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and Generative-AI apps and tool development.
+              <div className="tagline2">
+                I have doing coding and using the following technologies:
+              </div>
+            </ScrollAnimation>
+            
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt={stack.name} />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,154 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/Chat-pdf.png",
+    title: "Ai Pdf Chat",
+    description:
+      "A Generative-AI tool that chats with ur PDF data and answers ur questions.",
+    tech_stack: ["Streamlit", "Python", "Langchain", "Openai"],
+    github_url: "https://github.com/Gauravmane16/Chat-with-PDF",
+    demo_url: "https://ai-pdf-chat.streamlit.app/",
+  },
+  {
+    img: "/mera-parichay.png",
+    title: "Mera Parichay",
+    description:
+      "My Old Portfolio website",
+    tech_stack: ["ReactJs", "NodeJs", "TailBlocks"],
+    github_url: "https://github.com/Gauravmane16",
+    demo_url: "https://mera-parichay.netlify.app",
+  },
+  {
+    img: "/Cyber-shopee.png",
+    title: "Cyber Shopee",
+    description:
+      "An E-Commerce Website were u can buy all the electronics stuff's",
+    tech_stack: ["HTML", "CSS","Javascript","Bootstrap"],
+    github_url: "https://github.com/Gauravmane16/Cyber-Shopee",
+    demo_url: "https://gauravmane16.github.io/Cyber-Shopee/",
+  },
+  {
+    img: "/library-g.png",
+    title: "Gaurav's Library",
+    description:
+      "An Online Book selling website where u can buy affordable books.",
+    tech_stack: ["HTML", "CSS","Javascript","Bootstrap"],
+    github_url: "https://github.com/Gauravmane16/Online-Book-Selling-Website-",
+    demo_url: "https://gauravmane16.github.io/Online-Book-Selling-Website-/",
+  },
+  {
+    img: "/Compass.png",
+    title: "Compass App",
+    description:
+      "An app to detect ur current location and Guides U",
+    tech_stack: ["HTML", "CSS","Javascript","Bootstrap"],
+    github_url: "https://github.com/Gauravmane16/compass-app",
+    demo_url: "https://gauravmane16.github.io/compass-app/",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+ 
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/refs/heads/master/icons/streamlit/streamlit-original.svg",
+    name: "Streamlit",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/refs/heads/master/icons/linux/langchain-original.svg",
+    name: "Langchain",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/refs/heads/master/icons/linux/linux-original.svg",
+    name: "Linux",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/refs/heads/master/icons/csharp/csharp-original.svg",
+    name: "C#",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original.svg",
+  //   name: "NextJs",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  //   name: "Typescript",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+  //   name: "React Native",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+  //   name: "Firebase",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+  //   name: "Flask",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+  //   name: "Material UI",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+  //   name: "Figma",
+  // },
+  // {
+  //   img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/tailwindcss/tailwindcss-plain.svg",
+  //   name: "TailwindCSS",
+  // },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/refs/heads/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
